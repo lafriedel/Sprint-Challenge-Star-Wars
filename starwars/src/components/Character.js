53ae51aba98e8ea20e11f8385cc6f1ffe7fcd737
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-11 11:51:34 -0500

    More global styles, render more info from API

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Character = props => {
     return (
-        <li className="char-name">{props.char.name}</li>
-    )
+        <li className="char-name">{props.char.name}
+            <ul>
+                <li>Gender: {props.char.gender}</li>
+                <li>Eye Color: {props.char["eye_color"]}</li>
+                <li>Hair Color: {props.char["hair_color"]}</li>
+                <li># of movie appearances: {props.char.films.length + 1}</li>
+            </ul>
+        </li>
+    );
 }
 
 export default Character;
