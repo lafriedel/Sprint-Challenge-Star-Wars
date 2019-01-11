b196e606af778f1afd27ee06e9592888f4a3fc8f
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-11 11:15:00 -0500

    import and connect Character, CharacterList to App

--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Character from './Character';
 import './StarWars.css';
 
 const CharacterList = props => {
-
+    return (
+        <ul>
+            
+        </ul>
+    )
 }
 
 export default CharacterList;
