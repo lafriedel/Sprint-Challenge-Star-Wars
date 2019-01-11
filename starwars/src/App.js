d4f0b395fc491320223f6225f70c8a949a35cbaf
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-11 13:51:22 -0500

    Attempted pagination stretch, code is commented out

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -6,15 +6,19 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      starwarsChars: []
+      starwarsChars: [],
+      singleChar: {},
+      nextURL: "",
+      prevURL: "",
+      currentIndex: 0
     };
   }
 
   componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people');
+    this.getCharacters('https://swapi.co/api/people', 0);
   }
 
-  getCharacters = URL => {
+  getCharacters = (URL, index) => {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
@@ -23,12 +27,29 @@ class App extends Component {
         return res.json();
       })
       .then(data => {
-        this.setState({ starwarsChars: data.results });
+        this.setState({ 
+          starwarsChars: data.results, 
+          singleChar: data.results[index], 
+          nextURL: data.next, 
+          prevURL: data.previous
+         }, console.log(data.results[index]));
       })
       .catch(err => {
         throw new Error(err);
       });
   };
+
+  // getSingleCharacter = () => {
+  //   this.getCharacters(this.state.nextURL);
+  // }
+
+  // goToNextChar = () => {
+  //   this.setState({
+  //     currentIndex: this.state.currentIndex + 1,
+  //     singleChar: data.results[this.state.currentIndex + 1]
+  //   })
+  // }
+
 
   render() {
     return (
@@ -36,6 +57,9 @@ class App extends Component {
         <h1 className="Header">Star Wars Characters</h1>
         <CharacterList 
           starwarsChars={this.state.starwarsChars}
+          singleChar={this.state.singleChar}
+          nextURL={this.state.nextURL}
+          prevURL={this.state.prevURL}
         />
       </div>
     );
--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -3,12 +3,15 @@ import Character from "./Character";
 import "./StarWars.css";
 
 const CharacterList = props => {
+  
   return (
     <section className="character-list">
       <ul>
         {props.starwarsChars.map((char, index) => {
           return <Character char={char} key={index} />;
         })}
+        
+        {/* <a href={props.nextURL}>Next</a> */}
       </ul>
     </section>
   );
